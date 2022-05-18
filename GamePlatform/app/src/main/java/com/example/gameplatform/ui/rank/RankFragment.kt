40f21106e7351f18268152784542b49ed4c45434
DOGGY-SAINT <1123117528@qq.com>
2022-05-18 22:17:11 +0800

    Rank initial.

--- GamePlatform/app/src/main/java/com/example/gameplatform/ui/rank/RankFragment.kt
+++ GamePlatform/app/src/main/java/com/example/gameplatform/ui/rank/RankFragment.kt
@@ -28,10 +28,6 @@ class RankFragment : Fragment() {
         _binding = FragmentRankBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textRank
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
