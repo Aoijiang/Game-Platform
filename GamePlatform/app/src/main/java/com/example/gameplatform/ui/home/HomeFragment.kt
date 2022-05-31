77c1974cf0525ea6861de3f3e6ecb4f2841b61a8
Aoijiang <haiilly@outlook.com>
2022-05-31 12:09:00 +0800

    文件接口，协程配置

--- GamePlatform/app/src/main/java/com/example/gameplatform/ui/home/HomeFragment.kt
+++ GamePlatform/app/src/main/java/com/example/gameplatform/ui/home/HomeFragment.kt
@@ -1,24 +1,37 @@
 package com.example.gameplatform.ui.home
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.example.gameplatform.SpecificActivity
 import com.example.gameplatform.databinding.FragmentHomeBinding
+import kotlinx.coroutines.*
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
+import java.lang.Exception
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private lateinit var gameList: ArrayList<CardView>
+    private lateinit var coverList: ArrayList<ImageView>
+    private lateinit var typeList: ArrayList<TextView>
+    private lateinit var nameList: ArrayList<TextView>
+    private lateinit var disList: ArrayList<TextView>
+    private lateinit var scoreList: ArrayList<TextView>
+    private lateinit var resultList: ArrayList<JSONObject>
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -35,6 +48,12 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         gameList = ArrayList()
+        coverList = ArrayList()
+        typeList = ArrayList()
+        nameList = ArrayList()
+        disList = ArrayList()
+        scoreList = ArrayList()
+        resultList = ArrayList()
 
         val game_1 = binding.cardGame1
         val game_2 = binding.cardGame2
@@ -45,14 +64,50 @@ class HomeFragment : Fragment() {
         gameList.add(game_3)
         gameList.add(game_4)
 
-        for (i in 0..3)
-            gameList[i].setOnClickListener {
-                //传递游戏名字参数并跳转
-                val name = gameList[i].tag
-                val intent: Intent = Intent(activity, SpecificActivity::class.java)
-                intent.putExtra("gameName", name.toString())
-                startActivity(intent)
-            }
+        val cover_1 = binding.cover1
+        val cover_2 = binding.cover2
+        val cover_3 = binding.cover3
+        val cover_4 = binding.cover4
+        coverList.add(cover_1)
+        coverList.add(cover_2)
+        coverList.add(cover_3)
+        coverList.add(cover_4)
+
+        val type_1 = binding.gameType1
+        val type_2 = binding.gameType2
+        val type_3 = binding.gameType3
+        val type_4 = binding.gameType4
+        typeList.add(type_1)
+        typeList.add(type_2)
+        typeList.add(type_3)
+        typeList.add(type_4)
+
+        val name_1 = binding.gameName1
+        val name_2 = binding.gameName2
+        val name_3 = binding.gameName3
+        val name_4 = binding.gameName4
+        nameList.add(name_1)
+        nameList.add(name_2)
+        nameList.add(name_3)
+        nameList.add(name_4)
+
+        val dis_1 = binding.gameDisc1
+        val dis_2 = binding.gameDisc2
+        val dis_3 = binding.gameDisc3
+        val dis_4 = binding.gameDisc4
+        disList.add(dis_1)
+        disList.add(dis_2)
+        disList.add(dis_3)
+        disList.add(dis_4)
+
+        val score_1 = binding.score1
+        val score_2 = binding.score2
+        val score_3 = binding.score3
+        val score_4 = binding.score4
+        scoreList.add(score_1)
+        scoreList.add(score_2)
+        scoreList.add(score_3)
+        scoreList.add(score_4)
 
         return root
     }
@@ -60,7 +115,7 @@ class HomeFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         //获取游戏信息
-        getGame()
+        context?.let { getGame(it) }
     }
 
     override fun onDestroyView() {
@@ -68,11 +123,11 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    private fun getGame() {
-        val url = "http://106.15.6.161/allGame"
+    private fun getGame(context: Context) {
+        val baseUrl = "http://106.15.6.161/"
         //创建请求对象
         val request = Request.Builder()
-            .url(url)
+            .url(baseUrl + "allGame")
             .get()
             .build()
 
@@ -83,11 +138,40 @@ class HomeFragment : Fragment() {
                 }
 
                 override fun onResponse(call: Call, response: Response) {
-                    val result = response.body?.string().toString()
-                    val formResult = JSONObject(result)
-                    for(i in 0..3)
-                        Log.e(i.toString(),formResult[i.toString()].toString())
+                    CoroutineScope(Dispatchers.Main).launch {
+                        withContext(Dispatchers.IO) {
+                            val result = response.body?.string().toString()
+                            val formResult = JSONObject(result)
+                            for (i in 0..3) {
+                                val child = JSONObject(formResult[i.toString()].toString())
+                                resultList.add(child)
+                            }
+                        }
+                        try {
+                            for (i in 0..3) {
+                                val child = resultList[i]
+                                Glide.with(context)
+                                    .load(baseUrl + "getImage/" + child["cover"].toString())
+                                    .into(coverList[i])
+                                typeList[i].text = child["type"].toString()
+                                disList[i].text = child["describe"].toString()
+                                scoreList[i].text = child["rating"].toString()
+                                nameList[i].text = child["name"].toString()
+                                gameList[i].setOnClickListener {
+                                    //传递游戏名字参数并跳转
+                                    val name = child["name"].toString()
+                                    val intent = Intent(activity, SpecificActivity::class.java)
+                                    intent.putExtra("gameName", name)
+                                    Log.e("名字", name)
+                                    startActivity(intent)
+                                }
+                            }
+                        } catch (e: Exception) {
+                            Log.e("错误", e.stackTraceToString())
+                        }
+                    }
                 }
             })
     }
 }
+
--- GamePlatform/app/src/main/java/com/example/gameplatform/ui/personal/PersonalFragment.kt
+++ GamePlatform/app/src/main/java/com/example/gameplatform/ui/personal/PersonalFragment.kt
@@ -1,20 +1,37 @@
 package com.example.gameplatform.ui.personal
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.example.gameplatform.SpecificActivity
 import com.example.gameplatform.databinding.FragmentPersonalBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+import java.lang.Exception
 
 class PersonalFragment : Fragment() {
 
     private var _binding: FragmentPersonalBinding? = null
-    private lateinit var gameList:ArrayList<LinearLayout>
+    private lateinit var gameList: ArrayList<LinearLayout>
+    private lateinit var nameList: ArrayList<TextView>
+    private lateinit var iconList: ArrayList<ImageView>
+    private lateinit var resultList: ArrayList<JSONObject>
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -29,32 +46,96 @@ class PersonalFragment : Fragment() {
 
         _binding = FragmentPersonalBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        gameList= ArrayList()
+        gameList = ArrayList()
+        nameList = ArrayList()
+        iconList = ArrayList()
+        resultList = ArrayList()
 
-
-        val game_1=binding.game1
-        val game_2=binding.game2
-        val game_3=binding.game3
-        val game_4=binding.game4
+        val game_1 = binding.game1
+        val game_2 = binding.game2
+        val game_3 = binding.game3
+        val game_4 = binding.game4
         gameList.add(game_1)
         gameList.add(game_2)
         gameList.add(game_3)
         gameList.add(game_4)
 
-        for(i in 0..3)
-            gameList[i].setOnClickListener{
-                //传递游戏名字参数并跳转
-                val name=gameList[i].tag
-                val intent:Intent=Intent(activity,SpecificActivity::class.java)
-                intent.putExtra("gameName",name.toString())
-                startActivity(intent)
-            }
+        val name_1 = binding.name1
+        val name_2 = binding.name2
+        val name_3 = binding.name3
+        val name_4 = binding.name4
+        nameList.add(name_1)
+        nameList.add(name_2)
+        nameList.add(name_3)
+        nameList.add(name_4)
+
+        val icon_1 = binding.gameImage1
+        val icon_2 = binding.gameImage2
+        val icon_3 = binding.gameImage3
+        val icon_4 = binding.gameImage4
+        iconList.add(icon_1)
+        iconList.add(icon_2)
+        iconList.add(icon_3)
+        iconList.add(icon_4)
 
         return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        context?.let { getGame(it) }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getGame(context: Context) {
+        val baseUrl = "http://106.15.6.161/"
+        //创建请求对象
+        val request = Request.Builder()
+            .url(baseUrl + "allGame")
+            .get()
+            .build()
+
+        OkHttpClient().newCall(request)
+            .enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e("错误", e.stackTraceToString())
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        withContext(Dispatchers.IO) {
+                            val result = response.body?.string().toString()
+                            val formResult = JSONObject(result)
+                            for (i in 0..3) {
+                                val child = JSONObject(formResult[i.toString()].toString())
+                                resultList.add(child)
+                            }
+                        }
+                        try {
+                            for (i in 0..3) {
+                                val child = resultList[i]
+                                Glide.with(context)
+                                    .load(baseUrl + "getImage/" + child["icon"].toString())
+                                    .into(iconList[i])
+                                nameList[i].text = child["name"].toString()
+                                gameList[i].setOnClickListener {
+                                    //传递游戏名字参数并跳转
+                                    val name = child["name"].toString()
+                                    val intent = Intent(activity, SpecificActivity::class.java)
+                                    intent.putExtra("gameName", name)
+                                    Log.e("名字", name)
+                                    startActivity(intent)
+                                }
+                            }
+                        } catch (e: Exception) {
+                            Log.e("错误", e.stackTraceToString())
+                        }
+                    }
+                }
+            })
     }
 }
